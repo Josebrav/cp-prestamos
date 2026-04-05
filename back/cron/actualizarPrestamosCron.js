ea9d4f29e002588ecb5b831773983cf99aa6aae0
royalgames51 <royalgames2025@gmail.com>
2026-04-05 19:13:58 -0300

    mejorass

--- back/cron/actualizarPrestamosCron.js
+++ back/cron/actualizarPrestamosCron.js
@@ -1,16 +1,15 @@
 // cron/actualizarPrestamosCron.js
-// cron/actualizarPrestamosCron.js
 const cron = require('node-cron');
 const { actualizarPrestamosVencidos } = require('../src/controllers/calcularCuota');
 
 // 🔹 Ejecutar al iniciar la app
 (async () => {
   const cantidad = await actualizarPrestamosVencidos();
- console.log(`🚀 Inicio: Se actualizaron ${cantidad} préstamos vencidos.`);
+  console.log(`🚀 Inicio: Se actualizaron ${cantidad} préstamos vencidos.`);
 })();
 
-// 🔹 Programar ejecución diaria
-cron.schedule('*/5 * * * *', async () => {
+// 🔹 Programar ejecución cada 1 hora
+cron.schedule('0 * * * *', async () => {
   const cantidad = await actualizarPrestamosVencidos();
   console.log(`🔁 Cron: Se actualizaron ${cantidad} préstamos vencidos.`);
 }, {
